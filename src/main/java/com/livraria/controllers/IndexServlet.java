e08575e96d4068c82abb9b27574ba197b963e0d2
Lucas Barbosa Calixto <81984351+lucasclx@users.noreply.github.com>
2025-06-17 09:23:03 -0300

    first commit

--- src/main/java/com/livraria/controllers/IndexServlet.java
+++ src/main/java/com/livraria/controllers/IndexServlet.java
@@ -2,7 +2,6 @@ package com.livraria.controllers;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,8 +9,8 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet para página inicial.
  * Mapeado para a raiz ("") do contexto, redireciona para a página da loja.
+ * Configuração no web.xml
  */
-@WebServlet(urlPatterns = {"", "/index"})
 public class IndexServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     
